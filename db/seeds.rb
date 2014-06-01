1d9b4eccfacfd3aee361507841d90587f690fc51
Paul Dariye <paul.dariye@gmail.com>
2014-06-01 14:57:08 -0400

    re-seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 50.times do |i|
   p "creating course #{i}"
   Course.create(
-    course_name: "Course #{i + 1}",
+    course_name: "Course #{(0...8).map { (65 + rand(26)).chr }.join.capitalize}",
     course_code: "C#{i}#{i}",
     course_desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus malesuada bibendum lacus sit amet vulputate. Pellentesque accumsan magna at lectus sagittis, id facilisis velit vestibulum. Aenean hendrerit orci in tincidunt imperdiet. Nunc porttitor, lacus vitae malesuada posuere, nisi neque imperdiet dui, id placerat massa mauris et sapien. Sed luctus ligula varius metus pretium commodo. Cras elementum vestibulum mi sit amet elementum.",
     course_website: "www.course#{i + 1}.com")
